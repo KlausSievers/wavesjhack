73f897bf2c1e7cb31e67d646dcb15eef71657b22
Klaus Sievers <klau-es@gmx.de>
2017-12-07 14:42:04 +0100

    Node to post transactions

---
+++ src/main/java/de/hrw/waves/wavesjhacker/waves/pojo/Node.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2017 Hochschule Ruhr West (HRW), Bottrop, Germany
+ *
+ *           - ALL RIGHTS RESERVED -
+ *
+ * project:  WavesJHacker
+ *
+ * file:     Node.java 
+ *
+ * created:  07.12.2017
+ *
+ * author:   KS, AS
+ */
+package de.hrw.waves.wavesjhacker.waves.pojo;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import de.hrw.waves.wavesjhacker.waves.pojo.transactions.Transaction;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+/**
+ * Copied by
+ * https://github.com/wavesplatform/WavesJ/blob/master/src/main/java/com/wavesplatform/wavesj/Node.java
+ *
+ * only the required part and adjusted to own transaction class
+ *
+ */
+public class Node {
+
+  public static final String DEFAULT_NODE = "https://testnode1.wavesnodes.com";
+  private final CloseableHttpClient client = HttpClients.createDefault();
+
+  private final URI uri;
+
+  public Node() {
+    try {
+      this.uri = new URI(DEFAULT_NODE);
+    } catch (URISyntaxException e) {
+      // should not happen
+      throw new RuntimeException(e);
+    }
+  }
+
+  public String send(Transaction tx) throws IOException {
+    return parseResponse(exec(request(tx)), "id", String.class);
+  }
+
+  private HttpUriRequest request(Transaction tx) throws IOException {
+    HttpPost request = new HttpPost(uri + tx.getTransactionType().getEndpoint());
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+    String jsonTx = mapper.writeValueAsString(tx);
+    System.out.println(jsonTx); 
+
+    request.setEntity(new StringEntity(jsonTx));
+    request.setHeader("Content-Type", "application/json");
+    request.setHeader("Accept", "application/json");
+    return request;
+  }
+
+  private HttpResponse exec(HttpUriRequest request) throws IOException {
+    HttpResponse r = client.execute(request);
+    if (r.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+      String error = parseResponse(r, "message", String.class);
+      throw new IOException(error);
+    }
+    return r;
+  }
+
+  private static <T> T parseResponse(HttpResponse r, String key, Class<T> type) throws IOException {
+    ObjectMapper mapper = type == Long.class ? longObjectMapper() : new ObjectMapper();
+    return type.cast(parseResponse(r, mapper).get(key));
+  }
+
+  private static Map<String, Object> parseResponse(HttpResponse r, ObjectMapper mapper) throws IOException {
+    return mapper.readValue(r.getEntity().getContent(), Map.class);
+  }
+
+  private static ObjectMapper longObjectMapper() {
+    return new ObjectMapper().enable(DeserializationFeature.USE_LONG_FOR_INTS);
+  }
+
+}
--- src/main/java/de/hrw/waves/wavesjhacker/waves/pojo/transactions/TransactionType.java
+++ src/main/java/de/hrw/waves/wavesjhacker/waves/pojo/transactions/TransactionType.java
@@ -14,24 +14,30 @@
 package de.hrw.waves.wavesjhacker.waves.pojo.transactions;
 
 public enum TransactionType {
-  GENESIS(1),
-  PAYMENT(2),
-  ISSUE(3),
-  TRANSFER(4),
-  REISSUE(5),
-  BURN(6),
-  EXCHANGE(7),
-  LEASE(8),
-  LEASE_CANCEL(9),
-  ALIAS(10);
+  GENESIS(1, ""),
+  PAYMENT(2, "/assets/broadcast/order"),
+  ISSUE(3, "/assets/broadcast/issue"),
+  TRANSFER(4, "/assets/broadcast/transfer"),
+  REISSUE(5, "/assets/broadcast/reissue"),
+  BURN(6, "/assets/broadcast/burn"),
+  EXCHANGE(7, "/assets/broadcast/exchange"),
+  LEASE(8, ""),
+  LEASE_CANCEL(9, ""),
+  ALIAS(10, "");
 
   private final byte type;
+  private final String endpoint;
 
-  private TransactionType(int type) {
+  private TransactionType(int type, String endpoint) {
     this.type = (byte) type;
+    this.endpoint = endpoint;
   }
   
   public byte getType() {
     return type;
+  }
+
+  public String getEndpoint() {
+    return endpoint;
   }
 }
