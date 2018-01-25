da53e04579218d8789ee62a817f58ea0fc40ed05
Andree <as.scherin@hotmail.com>
2018-01-25 08:02:25 +0100

    Added Logback fileappender to log into log/wavesjhack.log

--- src/main/java/de/hrw/waves/wavesjhacker/Application.java
+++ src/main/java/de/hrw/waves/wavesjhacker/Application.java
@@ -20,7 +20,6 @@ import java.net.URISyntaxException;
 import com.wavesplatform.wavesj.PrivateKeyAccount;
 import de.hrw.waves.wavesjhacker.waves.pojo.Asset;
 import de.hrw.waves.wavesjhacker.waves.pojo.AssetPair;
-import de.hrw.waves.wavesjhacker.waves.pojo.Node;
 import de.hrw.waves.wavesjhacker.waves.pojo.Order;
 import de.hrw.waves.wavesjhacker.waves.pojo.OrderType;
 import de.hrw.waves.wavesjhacker.waves.pojo.transactions.ExchangeTranscation;
@@ -30,41 +29,42 @@ import de.hrw.waves.wavesjhacker.websocket.UnconfirmedExchangeTxListener;
 import de.hrw.waves.wavesjhacker.websocket.WavesMessageHandler;
 import de.hrw.waves.wavesjhacker.websocket.WebsocketClientEndpoint;
 import de.hrw.waves.wavesjhacker.websocket.pojo.WavesWsMessage;
-import java.io.IOException;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
 
-  private static long HOUR = 24 * 3_600_000 * 10;
-  
+  private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+  private static final long HOUR = 24 * 3_600_000 * 10;
+
   public static void main(String[] args) throws URISyntaxException {
 //    initWsConnection();
- byte[] bytes = Base58.decode("WA1QuEjJX2b6w1xKGaqyFZwPDN9DMiAoTYVfe2TUyd7UUTmqmTkswJmCWfZJEDANAnPfjENZJqE6HN1t5MVLTWU");
-    System.out.println(bytes.length);
-    System.out.println("Decode: " + String.valueOf(Base58.decode("8LQW8f7P5d5PZM7GtZEBgaqRPGSzS3DfPuiXrURJ4AJS")));
-    sendTestExchangeTx();
+//    byte[] bytes = Base58.decode("WA1QuEjJX2b6w1xKGaqyFZwPDN9DMiAoTYVfe2TUyd7UUTmqmTkswJmCWfZJEDANAnPfjENZJqE6HN1t5MVLTWU");
+//    System.out.println(bytes.length);
+//    System.out.println("Decode: " + String.valueOf(Base58.decode("8LQW8f7P5d5PZM7GtZEBgaqRPGSzS3DfPuiXrURJ4AJS")));
+//    sendTestExchangeTx();
+    LOGGER.info("Application started");
   }
 
   private static void initWsConnection() throws URISyntaxException {
     URI uri = new URI("ws://ws.wavesplatform.com/api");
     WavesMessageHandler wavesMessageHandler = new WavesMessageHandler();
     wavesMessageHandler.addUtxListener(new UnconfirmedExchangeTxListener());
-    
+
     final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(uri, wavesMessageHandler);
     clientEndPoint.sendMessage(WavesWsMessage.SUBSCRIBE_UTX);
   }
 
   private static void sendTestExchangeTx() {
-    PrivateKeyAccount matcher =PrivateKeyAccount.fromSeed(
-            "pistol assist festival craft library force orphan amateur bullet scissors abstract among leisure hamster model", 0, Account.TESTNET);
+    PrivateKeyAccount matcher = PrivateKeyAccount.fromSeed(
+            "pistol assist festival craft library force orphan amateur bullet scissors abstract among leisure hamster model", 0,
+            Account.TESTNET);
     PrivateKeyAccount klaus = PrivateKeyAccount.fromSeed(
             "evidence unit market inject swamp quote just know control equal file avoid metal scout video", 0, Account.TESTNET);
     PrivateKeyAccount andree = PrivateKeyAccount.fromSeed(
-            "pistol assist festival craft library force orphan amateur bullet scissors abstract among leisure hamster model", 0, Account.TESTNET);
+            "pistol assist festival craft library force orphan amateur bullet scissors abstract among leisure hamster model", 0,
+            Account.TESTNET);
 
     Date now = new Date();
 
@@ -74,7 +74,7 @@ public class Application {
     order.setPrice(1);
     order.setMatcherFee(1);
     order.setSenderKey(klaus.getPublicKey());
-    order.setMatcherKey(matcher.getPublicKey()); 
+    order.setMatcherKey(matcher.getPublicKey());
     order.setExpiration(now.getTime() + HOUR);
     order.setTimestamp(now.getTime());
     order.setAssetPair(new AssetPair(Asset.WAVES, Asset.BTC));
@@ -86,7 +86,7 @@ public class Application {
     order2.setPrice(1);
     order2.setMatcherFee(1);
     order2.setSenderKey(andree.getPublicKey());
-    order2.setMatcherKey(matcher.getPublicKey()); 
+    order2.setMatcherKey(matcher.getPublicKey());
     order2.setExpiration(now.getTime() + HOUR);
     order2.setTimestamp(now.getTime());
     order2.setAssetPair(new AssetPair(Asset.WAVES, Asset.BTC));
@@ -94,7 +94,7 @@ public class Application {
 
     Transaction testTx = new ExchangeTranscation(order, order2, 1, 100000000, 300000);
     testTx.updateSignature(matcher);
-    
+
     boolean b = Signature.cipher.verifySignature(matcher.getPublicKey(), testTx.getDataToSign().array(), testTx.getSignature());
     System.out.println("OUR FUCKING TEST" + b);
 
